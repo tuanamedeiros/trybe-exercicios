ee443ddb7b359e2bf07f78e129773e86aa1d06a7
Tuana Medeiros <tuanamari@hotmail.com>
2021-11-29 11:44:18 -0300

    adicionando os 4 primeiros exercicios

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercicio2.js
@@ -0,0 +1,9 @@
+const a = 2
+const b = 5
+
+if (a > b) {
+    console.log("a é maior que b")
+}
+else {
+    console.log("b é maior que a")
+}
---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercicio3.js
@@ -0,0 +1,13 @@
+const a = 10
+const b = 2
+const c = 20
+
+if (a > b && a > c) {
+    console.log("a é o maior")
+}
+else if (b > a && b > c) {
+    console.log("b é o maior")
+}
+else {
+    console.log("o maior é c")
+}
